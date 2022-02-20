f508e7bc1add37c588a4b6e9fbbaf31c98a8be63
jaydip-lnxct <jaydeep@lnxct.com>
2022-02-20 10:40:52 +0530

    JSX implements

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Name=()=>{
+  return(
+    <ul style={{textAlign:'center'}}>
+      <h1 style={{color: "red"}}>Items</h1>
+      <li>1.Pizza</li>
+      <li>2.Manchurian</li>
+      <li>3.Dosa</li>
+      <li>4.Gujarati Dish</li>
+      <li>5.Pau Bhaji</li>
+    </ul>
+  )
+}
+
+
+
+
+ReactDOM.render(<Name/>, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
